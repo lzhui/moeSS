2b017ddc7e49361b317ba42bc3607fc84166f746
linzenghui <linzenghui@tdnnic.org>
2016-10-25 17:53:29 +0800

    提交

---
+++ application/controllers/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lzhui
+ * Date: 2016/10/25
+ * Time: 15:28
+ */
+class Test extends CI_Controller
+{
+    public function index() {
+        $this->load->view('test');
+//        echo "Hello World!";
+    }
+
+}
